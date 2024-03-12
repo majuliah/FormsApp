e6e5102ee02259205be4d55cd3633763356761f6
Majulia <majulialcarvalho@gmail.com>
2024-03-11 22:02:10 -0300

    finalizando cap + exercicios

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,24 @@ namespace FormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            double saldo = 100.0;
+            double saque = 10;
+            saldo -= saque; //implementando comentários
+
+            //MessageBox.Show($"O saldo da conta é {saldo}");
+
+            //exercicio calcular media idades
+            int idadeMaju = 24;
+            int idadeAlex = 25;
+            int idadeGabi = 26;
+
+            int mediaIdades = (idadeMaju + idadeAlex + idadeGabi) / 3;
+            MessageBox.Show($"A média das idades é {mediaIdades}");
+
+            //O que acontece com o código?
+            int pi = (int)3.14;
+            //MessageBox.Show($"Valor de pi= {pi}");
+
         }
     }
 }
